fe59e7be0be9b7d8bbdad1ad131cf1dfb75f215d
Euwehh <85025777+9lan@users.noreply.github.com>
2024-06-02 00:10:41 +0700

    chore: initial commit

---
+++ android/src/main/java/com/9lan/reactnativeble/ReactNativeBleModule.java
@@ -0,0 +1,32 @@
+package com.9lan.reactnativeble;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.module.annotations.ReactModule;
+
+@ReactModule(name = ReactNativeBleModule.NAME)
+public class ReactNativeBleModule extends ReactContextBaseJavaModule {
+  public static final String NAME = "ReactNativeBle";
+
+  public ReactNativeBleModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+  }
+
+  @Override
+  @NonNull
+  public String getName() {
+    return NAME;
+  }
+
+
+  // Example method
+  // See https://reactnative.dev/docs/native-modules-android
+  @ReactMethod
+  public void multiply(double a, double b, Promise promise) {
+    promise.resolve(a * b);
+  }
+}
---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  ReactNativeBleExample
+//
+
+import Foundation
